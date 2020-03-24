90a7a2ae3db4f05481eed989854d0dc6706e1566
Jessica Hairston <hairstonjessica@aol.com>
2020-03-23 20:18:29 -0500

    Initial Commit

---
+++ assignment6/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment6
+//
+//  Created by Jessica Hairston on 3/23/20.
+//  Copyright © 2020 group3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
